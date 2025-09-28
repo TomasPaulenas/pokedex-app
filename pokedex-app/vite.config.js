89bb18e8ea5ea70c90d43e8df065e8aad5607c05
Tomas <tomaspaulenas@hotmail.com>
2025-09-28 13:53:28 +0200

    add vite.config.js for Vercel build

---
+++ pokedex-app/vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+// Config mínima para que Vercel construya y publique tu app
+export default defineConfig({
+  build: {
+    outDir: 'dist'
+  }
+})
+  
